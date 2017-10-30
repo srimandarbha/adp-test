1501fe9633809dc6f45af79df0890ecbf06bd016
sriman <sriman1988@gmail.com>
2017-10-30 14:11:10 +0530

    vagrant.tf

---
+++ task2/vagrant.tf
@@ -0,0 +1,30 @@
+resource "virtualbox_vm" "node" {
+    count = 2
+    name = "${format("node-%02d", count.index+1)}"
+    url = "https://atlas.hashicorp.com/ubuntu/boxes/trusty64/versions/14.04/providers/virtualbox.box"
+    image = "./virtualbox-ubuntu.box"
+    cpus = 2
+    memory = "512mib"
+
+    network_adapter {
+        type = "bridged"
+        host_interface = "en0"
+    }
+    provisioner "local-exec" {
+        inline = [
+            "touch /tmp/root_setup.sh",
+        ]
+        connection {
+            type     = "ssh"
+            user     = "vagrant"
+            password = "vagrant"
+        }
+    }
+
+}
+output "IPAddr" {
+    value = "${element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)}"
+}
+output "IPAddr_2" {
+    value = "${element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)}"
+}
